f51b1ee3886f688ae32e9bfd06c382416a32322f
chyuhung <977599667@qq.com>
2022-09-09 14:51:35 +0800

    格式修改

--- main.go
+++ main.go
@@ -12,6 +12,13 @@ import (
 	"sync"
 )
 
+func isTrue(i int) bool {
+	if i == 0 {
+		return false
+	}
+	return true
+}
+
 func main() {
 	var wg sync.WaitGroup
 	var hostList []*Host.Host
@@ -23,11 +30,12 @@ func main() {
 	var maxNum = flag.Int("m", 10, "The number of goroutines, the maximum is the number of IPs.")
 	//ping包数量
 	var pingNum = flag.Int("n", 1, "Number of messages per ping.")
-	var isICMP = flag.Bool("t", true, "Set the type of ping pinger will send.\n"+
+	var isICMP = flag.Int("t", 1, "Set the type of ping pinger will send.\n"+
 		"False means pinger will send an \"unprivileged\" UDP ping.\n"+
 		"True means pinger will send a \"privileged\" raw ICMP ping.\n"+
 		"NOTE: setting to true requires that it be run with super-user privileges.")
 	flag.Parse()
+
 	fmt.Println("Goroutines:", *maxNum)
 	fmt.Println("Messages:", *pingNum)
 	fmt.Println("Input file:", *input)
@@ -50,7 +58,7 @@ func main() {
 		h := Host.NewHost(ip)
 		hostList = append(hostList, h)
 		wg.Add(1)
-		go h.Run(&wg, *pingNum, *isICMP, maxg)
+		go h.Run(&wg, *pingNum, isTrue(*isICMP), maxg)
 	}
 	wg.Wait()
 	f, err := os.Create(*output)
